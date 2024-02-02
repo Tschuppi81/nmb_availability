fae2bd017b088ed38a13cb5d6755363617731235
Reto Tschuppert <reto.tschuppert@seantis.ch>
2024-02-02 23:19:08 +0100

    Order imports

--- src/villa_availabitlity/main.py
+++ src/villa_availabitlity/main.py
@@ -1,6 +1,6 @@
 from src.villa_availabitlity.common import print_availability_results
-from src.villa_availabitlity.nmb import scrape_nmb
 from src.villa_availabitlity.intervillas import scrape_intervillas
+from src.villa_availabitlity.nmb import scrape_nmb
 
 
 if __name__ == "__main__":
